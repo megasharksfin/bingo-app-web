5238d980cca466c099e802e35d0e72e9b0728a0c
megasharksfin <53161366+megasharksfin@users.noreply.github.com>
2020-09-07 20:08:13 +0800

    reverted app.js

--- server.js
+++ server.js
@@ -1,8 +1,49 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/users');
+const gameRoutes = require('./routes/game');
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use('/api/users', userRoutes);
+app.use('/api/game', gameRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: 'Error. Bad request'
+    }
+  });
+}); 
 
 const port = process.env.port || 5000;
 
-const server = http.createServer(app);
+app.listen(port);
 
-server.listen(port);
+module.exports = app;
