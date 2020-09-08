7852df908f2038b77c71441b6cb74229d823d5e5
megasharksfin <53161366+megasharksfin@users.noreply.github.com>
2020-09-08 18:49:00 +0800

    added env variables

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "us-cdbr-east-02.cleardb.com",
-  USER: "b75db007ef6067",
-  PASSWORD: "6ad296fd",
-  DB: "heroku_74c8b7ce5a9f540"
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB
 };
